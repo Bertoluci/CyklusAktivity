3066a867b4ce271d79c9ab26ce0ef7f868a1ae75
Robert Jedek <robert.jedek@gmail.com>
2018-10-08 19:25:13 +0200

    ukladani a obnoveni stavu aktivity

--- app/src/main/java/cz/seucit/android/cyklusaktivity/StateChangeActivity.java
+++ app/src/main/java/cz/seucit/android/cyklusaktivity/StateChangeActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class StateChangeActivity extends AppCompatActivity {
 
@@ -72,12 +73,18 @@ public class StateChangeActivity extends AppCompatActivity {
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         Log.i(LAD, "onSaveInstanceState");
+        final EditText textBox = (EditText) findViewById(R.id.editText);
+        CharSequence sText = textBox.getText();
+        outState.putCharSequence("UlozenyText", sText);
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         Log.i(LAD, "onRestoreInstanceState");
+        final EditText textBox = (EditText) findViewById(R.id.editText);
+        CharSequence sText = savedInstanceState.getCharSequence("UlozenyText");
+        textBox.setText(sText);
     }
 
 
